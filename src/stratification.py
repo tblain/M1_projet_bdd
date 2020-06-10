586e1f494fa814d939e7ffe396a8c27fe6e92fdc
tblain <tblain69@gmail.com>
2020-06-10 17:09:40 +0200

    fix strat

--- src/stratification.py
+++ src/stratification.py
@@ -1,30 +1,45 @@
 def stratify(edb, idb):
-    stratum = {}
+    stratum_dic = {}
 
     # init
     for d in edb:
-        stratum[d] = 1
+        stratum_dic[d] = 1
 
     for d in idb:
-        stratum[d] = 1
+        stratum_dic[d] = 1
+
+    print(stratum_dic)
 
     no_changes = False
+
+    max_level = 0
 
     # stratification
     while(not no_changes):
         no_changes = True
 
         for l_pred in idb:
-            for rule in idb[l_pred]:
-                for r_pred in rule:
-                    if r_pred[1]:
-                        stratum[r_pred[0]]
-                        new_val = max(stratum[l_pred], stratum[r_pred[0]] + 1)
-                    else:
-                        new_val = max(stratum[l_pred], stratum[r_pred[0]])
+            rule = idb[l_pred][0]
+            for r_pred in rule:
+                if r_pred[1]:
+                    new_val = max(stratum_dic[l_pred], stratum_dic[r_pred[0]] + 1)
+                else:
+                    new_val = max(stratum_dic[l_pred], stratum_dic[r_pred[0]])
 
-                    if new_val != stratum[l_pred]:
-                        stratum[l_pred] = new_val
-                        no_changes = False
+                if new_val != stratum_dic[l_pred]:
+                    stratum_dic[l_pred] = new_val
+                    max_level = max(max_level, new_val)
+                    no_changes = False
 
+    print("stratum_dic: ", stratum_dic)
+
+    stratum = []
+    for i in range(max_level):
+        stratum.append([])
+
+    for r in stratum_dic:
+        if not r in edb:
+            level = stratum_dic[r]
+            stratum[level-1].append(r)
+    
     return stratum
